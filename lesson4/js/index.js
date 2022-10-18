124e3ee4c3e6edb23c70ed6894ac3f912502543a
MOCAHE <can22002@byui.edu>
2022-10-18 03:28:29 -0500

    Cambios

---
+++ lesson4/js/index.js
@@ -0,0 +1,13 @@
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
+
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.querySelector("#year").textContent = Year;
+
+
+// Función para la fecha
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
