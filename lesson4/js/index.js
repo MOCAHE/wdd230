de35039db9d9cbb1e53e4bc344c1cff98d51263c
MOCAHE <can22002@byui.edu>
2022-10-18 21:18:09 -0500

    Hasta aquí casi todo bien

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -11,3 +11,12 @@ const datefield = document.querySelector(".date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// Función del Botón de Hamburguesa
+function toggleMenu (){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
