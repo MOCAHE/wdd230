6408aeaec184d09b2c212a7435d3a63c0f1838d3
MOCAHE <can22002@byui.edu>
2023-07-13 04:59:19 -0600

    Ajustes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,12 @@ document.querySelector("#update").textContent = Update;
 let currentDate = new Date();
 let Year = currentDate.getFullYear();
 document.querySelector("#year").textContent = Year;
+
+// Función del Botón de Hamburguesa
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
