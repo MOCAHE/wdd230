c4475205c8ada243c862c2539256bea78563d6e1
MOCAHE <can22002@byui.edu>
2023-11-16 15:00:24 -0600

    Nuevo archivo js

---
+++ scripts/hamburgerBotton.js
@@ -0,0 +1,23 @@
+// Función del Botón de Hamburguesa
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+// Controlador para cambio de tamaño de ventana
+function handleWindowSizeChange() {
+    const nav = document.getElementById("primaryNav");
+    const hamburgerBtn = document.getElementById("hamburgerBtn");
+
+    if (window.innerWidth > 500) { // Asumiendo 500px como umbral para pantallas móviles
+        nav.classList.remove("open");
+        hamburgerBtn.classList.remove("open");
+    }
+}
+
+// Asignar eventos
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+// Escuchar cambios en el tamaño de la ventana
+window.onresize = handleWindowSizeChange;
