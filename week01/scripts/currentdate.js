4abc0652fb4862722926720f117c209f65752e26
MOCAHE <can22002@byui.edu>
2023-11-17 04:08:18 -0600

    Correción archivo JS en chamber site plan

--- week01/scripts/currentdate.js
+++
@@ -1,6 +0,0 @@
-const Update = document.lastModified
-document.querySelector("#update").textContent = Update;
-
-let currentDate = new Date();
-let Year = currentDate.getFullYear();
-document.querySelector("#year").textContent = Year;
