85399168b6b797c3da2d191082ba92e15b7c9857
MOCAHE <can22002@byui.edu>
2023-11-19 02:26:51 -0600

    Cambios a chamber

--- chamber/scripts/hamburguerBotton2.js
+++ chamber/scripts/hamburguerBotton2.js
@@ -1,11 +1,3 @@
-const Update = document.lastModified
-document.querySelector("#update").textContent = Update;
-
-let currentDate = new Date();
-let Year = currentDate.getFullYear();
-document.querySelector("#year").textContent = Year;
-
-
 // Función para la fecha
 const datefield = document.querySelector(".date");
 const now = new Date();
@@ -13,7 +5,7 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 // Función del Botón de Hamburguesa
-function toggleMenu (){
+function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
