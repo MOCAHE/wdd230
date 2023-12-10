2af248d0b44cb1561c98542ec5e853617c9cc564
MOCAHE <can22002@byui.edu>
2023-12-09 23:54:50 -0600

    Join finished

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,13 @@ document.querySelector("#update").textContent = Update;
 let currentDate = new Date();
 let Year = currentDate.getFullYear();
 document.querySelector("#year").textContent = Year;
+
+
+// JOIN PAGE
+document.addEventListener('DOMContentLoaded', function() {
+var currentDate = new Date();
+
+document.getElementById('date').value = currentDate.toISOString().slice(0, 10);
+document.getElementById('time').value = currentDate.toTimeString().slice(0, 8);
+document.getElementById('timestamp').value = currentDate.toISOString();
+});
